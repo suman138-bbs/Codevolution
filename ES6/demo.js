e185a32639d4ea25d97e34df4eba899c18e593a5
Suman Bhandari <sumanbhandari9860@gmail.com>
2023-04-18 23:01:53 +0530

    learningEs61

---
+++ ES6/demo.js
@@ -0,0 +1,22 @@
+// Any var declaration going to the top of the Function
+
+// let have no hoisting and block scope
+
+//////////////////////////////////
+// for (var i = 0; i < 5; i++){
+//     setTimeout(function () {
+//         console.log(i)
+//     },1000)
+// }
+
+//////////////////////////////////////
+
+// const ob1 = {
+//     name:"Suman"
+// }
+
+// ob1.name = "Bhandari in India"
+// console.log(ob1.name)
+
+//////////////////////////////////////////////////
+
