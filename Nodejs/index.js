585e94ed57c53d91c053330cc68dcce5c44f52d5
Suman Bhandari <sumanbhandari9860@gmail.com>
2023-04-18 20:27:49 +0530

    Learning

---
+++ Nodejs/index.js
@@ -0,0 +1,13 @@
+// const add = require('./add');
+// console.log(add(2,3))
+// console.log("index.js file")
+// console.log(add(4,5))
+
+// const math = require('./math');
+// console.log(math)
+// console.log(math.add(2, 3));
+
+
+///////////////////////////////////////////////////
+const data = require('./data.json')
+console.log(data)
