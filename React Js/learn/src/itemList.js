800a65aac6e11b38103426d00e4c56cf989bb00d
Suman Bhandari <sumanbhandari9860@gmail.com>
2023-04-29 14:00:44 +0530

    React

--- React Js/learn/src/itemList.js
+++ React Js/learn/src/itemList.js
@@ -1,24 +1,11 @@
 import React from 'react'
-import {FaTrashAlt} from 'react-icons/fa'
+import LineItem from './LineItem'
 const ItemList = ({items,handleCheck,handleDelete}) => {
   return (
       <main>
           <ul>
           {items.map((item) => {
-            return (<li className="item" key={item.id}>
-              <input
-                type="checkbox"
-                checked={item.checked}
-                onChange={() => handleCheck(item.id)}
-              />
-              <label
-                style={item.checked ? { textDecoration: 'line-through' } : null}
-                onDoubleClick={() => handleCheck(item.id)}
-            
-              >{item.item}</label>
-              <FaTrashAlt role="button" tabIndex="0" onClick={() =>
-                handleDelete(item.id)} />
-            </li>)
+            return <LineItem key={item.id} item={item} handleCheck={handleCheck} handleDelete={handleDelete}/>
           })}
         </ul>
     </main>
