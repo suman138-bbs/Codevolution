b9299214442875dfea50d0b48e5866971aa9b36e
Suman Bhandari <sumanbhandari9860@gmail.com>
2023-05-16 18:41:19 +0530

    TypeScript

---
+++ TypeScript/build/js/main.js
@@ -0,0 +1,52 @@
+"use strict";
+/**
+ * Basics
+ */
+// let username:string = "Suman Bhandari"
+// console.log(username)
+// let a:number = 12
+// let b: string | number;
+// let c: number = 2
+// b=2
+// console.log(a/b)
+// let stringArr:string[] = ['one', 'two', 'three']
+// let giutars=['start',3565]
+// let mixedData = ['EVH', true]
+// stringArr.push('hey')
+// Tuple
+// let myTuple: [string, number, boolean] = ["suman", 22, true]
+// interface Guitarist  {
+//     name: string,
+//     active?: boolean,
+//     albums:(string | number)[]
+// }
+// let evh: Guitarist = {
+//     name: 'Eddle',
+//     active: false,
+//     albums:[2]
+// }
+// Type
+// type stringorNum = string | number;
+// type stringornumarr = (string | number)[]
+// interface Guitarist  {
+//     name?: string,
+//     active: boolean,
+//     albums:stringornumarr
+// }
+// let ob: Guitarist = {
+//     name: "Suman",
+//     active: true,
+//     albums:[1,2,3,"Five"]
+// }
+// console.log(ob.active)
+// let username: 'Dave' | 'John' | 'Any'
+// username = 'Any'
+// Functions
+const add = (a, b) => {
+    return a + b;
+};
+const logmsg = (message) => {
+    console.log(message);
+};
+console.log(add(2, 3));
+logmsg("Suman Bhandari");
